0812a10a12178c1637b04dd735112bb5ea5bcd33
vipulc8 <vipul.c@simformsolutions.com>
2023-01-20 18:15:50 +0530

    Create UI

---
+++ app.js
@@ -0,0 +1,15 @@
+// const inptext = document.querySelector("input");
+
+// let txt = "";
+
+// const num = document.getElementById("7");
+// num.addEventListener("click", () => {
+//   console.log(num.innerText);
+//   inptext.value += num.innerText;
+// });
+
+// const num8 = document.getElementById("8");
+// num8.addEventListener("click", () => {
+//   console.log(num8.innerText);
+//   inptext.value += num8.innerText;
+// });
