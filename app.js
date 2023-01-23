c64ae9695a469e4205bc53180de9c0fbe447d240
vipulc8 <vipul.c@simformsolutions.com>
2023-01-23 18:38:38 +0530

    Add icon

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-// const inptext = document.querySelector("input");
+// const intext = document.querySelector("input");
 
-// let txt = "";
+// // let txt = "";
 
-// const num = document.getElementById("7");
-// num.addEventListener("click", () => {
-//   console.log(num.innerText);
-//   inptext.value += num.innerText;
-// });
+// // const num = document.getElementById("7");
+// // num.addEventListener("click", () => {
+// //   console.log(num.innerText);
+// //   inptext.value += num.innerText;
+// // });
 
-// const num8 = document.getElementById("8");
-// num8.addEventListener("click", () => {
-//   console.log(num8.innerText);
-//   inptext.value += num8.innerText;
-// });
+// // const num8 = document.getElementById("8");
+// // num8.addEventListener("click", () => {
+// //   console.log(num8.innerText);
+// //   inptext.value += num8.innerText;
+// // });
+
+// const btn =document.querySelectorAll('.btn');
